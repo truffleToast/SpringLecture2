6c804a846f1ca9c4f416ce6969f0662960f4d6d2
truffleToast <kimchi98125@gmail.com>
2023-11-04 00:12:49 +0900

    study 0,0,1

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/f0/60a83577587a001e1965ac5696f7fea4
@@ -0,0 +1,10 @@
+package com.in28minutes.learnspringframework;
+
+public class AppGamingBasicJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -0,0 +1,15 @@
+package com.in28minutes.learnspringframework;
+
+public class AppGamingBasicJava {
+
+	public static void main(String[] args) {
+		// Mario Game
+		
+		//var marioGame = new MarioGame();
+		var superContraGame = new SuperContraGame();
+		var ganeRunner = new GameRunner(superContraGame);
+		ganeRunner.run();
+		
+	}
+
+}
